3088b6dfa38405493fe9dd6dfbd13a2a6a16b059
TunaSandwinch <winch.mascarenas@gmail.com>
2024-02-14 16:43:53 +0800

    added the first condition in the if else of the game

--- script.js
+++ script.js
@@ -13,12 +13,28 @@ function chooseSide(){
     return side
 }
 
-
-
+let Cscore = 0;
+let Pscore = 0;
 for (let i = 0; i < 5 ;){
     i++
+    let playerChoice = prompt("Choose! : rock, paper, or scisssor");
     let Compchoice = chooseSide();
     console.log(Compchoice);
+    console.log(playerChoice)
+
+    if(playerChoice.toLocaleLowerCase() == "rock"){
+        console.log("code pass to 1st if");
+        if(Compchoice == "scissor"){
+           Pscore+=1;
+           alert(`Computer: ${Compchoice} , Player Choice: ${playerChoice} You WIN! Score: ${Cscore} / ${Pscore}`);
+        }else if(Compchoice == "paper"){
+            Cscore+=1;
+            alert(`Computer: ${Compchoice} , Player Choice: ${playerChoice} You Lose! Score: ${Cscore} / ${Pscore}`);
+        }else{
+            alert(`Computer: ${Compchoice} , Player Choice: ${playerChoice} Its A Tie! Score: ${Cscore} / ${Pscore}`);
+        }
+
+    } 
     
 }
 
